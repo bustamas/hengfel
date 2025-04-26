d7362e0bdd05dd70c8c30a16c291cdb657a77200
Bús Tamás <bustamas555@gmail.com>
2025-04-26 20:12:40 +0200

    kezdés

---
+++ bs-config.js
@@ -0,0 +1,14 @@
+/*
+ * Készítette: Az Ön neve
+ * Dátum: 2024-04-26
+ * Feladat: Browser-sync konfigurációs fájl
+*/
+
+module.exports = {
+    server: {
+      baseDir: "public"
+    },
+    files: ["public/**/*.*"],
+    port: 3000,
+    open: true
+  };  
